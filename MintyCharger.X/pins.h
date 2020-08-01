3a02d1c93da4e8ec311cddb697f9e16a4b011e09
Nathan Campos <eeepc904@gmail.com>
2020-08-01 15:03:38 +0100

    Defined the pins

---
+++ MintyCharger.X/pins.h
@@ -0,0 +1,23 @@
+/* 
+ * pins.h
+ * Pin definitions.
+ * 
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+#ifndef PINS_H
+#define	PINS_H
+
+// Pin definitions.
+#define BTN_SELECT (1 << 3)  // RA3
+#define DISCH_EN   (1 << 4)  // RA4
+#define BOOST_PWM  (1 << 5)  // RA5
+#define VSENSE     1         // RC0
+#define ISENSE     (1 << 1)  // RC1
+#define CHG_LED0   (1 << 2)  // RC2
+#define CHG_LED1   (1 << 3)  // RC3
+#define CHG_LED2   (1 << 4)  // RC4
+#define CHG_LED3   (1 << 5)  // RC5
+
+#endif	/* PINS_H */
+
