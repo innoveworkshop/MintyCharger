67db4e2db100478c141af3e5f748e62b20200a80
Nathan Campos <eeepc904@gmail.com>
2020-08-01 15:18:37 +0100

    Setting up the digital I/O pins

--- MintyCharger.X/main.c
+++ MintyCharger.X/main.c
@@ -20,13 +20,51 @@
 #include <xc.h>
 #include <pic16f18325.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include "pins.h"
+
+// Function prototypes.
+void SetupPins();
 
 /**
  * Application main entry point.
  */
 void main(void) {
+	uint8_t pins = CHG_LED0;
+	
+	// Initialize pins.
+	SetupPins();
+	
 	while (true) {
+		LATC = ~pins;
+		
+		if (pins < CHG_LED3) {
+			pins <<= 1;
+		} else {
+			pins = CHG_LED0;
+		}
+		
+		__delay_ms(500);
 	}
 
 	return;
+}
+
+/**
+ * Sets up all the pins for the application.
+ */
+void SetupPins() {
+	// Setup digital inputs.
+	TRISA = 0b111 + BTN_SELECT;
+	TRISC = VSENSE + ISENSE;
+	
+	// Setup pull-ups.
+	WPUC = 0;
+	
+	// Setup open-drains.
+	ODCONC = CHG_LED0 + CHG_LED1 + CHG_LED2 + CHG_LED3;
+	
+	// Set all the outputs to LOW.
+	LATA = 0;
+	LATC = 0;
 }
