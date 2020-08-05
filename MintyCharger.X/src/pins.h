c863a3abde4b010268eb8a10f4e7d81a4460197b
Nathan Campos <eeepc904@gmail.com>
2020-08-05 14:03:31 +0100

    Moved the pin mapping graphics to the appropriate header file

--- MintyCharger.X/src/pins.h
+++ MintyCharger.X/src/pins.h
@@ -8,8 +8,21 @@
 #ifndef PINS_H
 #define	PINS_H
 
+//                           16F18325
+//                         +----------+
+// LED Shift Register DAT -|RA0    RC0|- Voltage Sense - ANC0
+// LED Shift Register CLK -|RA1    RC1|- Current Sense - ANC1
+// LED Shift Register LAT -|RA2    RC2|- Charge LED 0
+// Select Switch          -|RA3    RC3|- Charge LED 1
+// Discharge Enable       -|RA4    RC4|- Charge LED 2
+// Boost DC/DC PWM - PWM1 -|RA5    RC5|- Charge LED 3
+//                         +----------+
+
+
 // Pin definitions.
-#define TX         (1 << 2)  // RA2
+#define SR_DATA    1         // RA0
+#define SR_CLOCK   (1 << 1)  // RA1
+#define SR_LATCH   (1 << 2)  // RA2
 #define BTN_SELECT (1 << 3)  // RA3
 #define DISCH_EN   (1 << 4)  // RA4
 #define BOOST_PWM  (1 << 5)  // RA5
@@ -25,4 +38,3 @@
 #define ADC_CH_ISENSE 0b10001
 
 #endif	/* PINS_H */
-
