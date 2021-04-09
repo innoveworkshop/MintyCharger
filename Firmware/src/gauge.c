c16f76591cfafbc2932faeb3b6d54718221c8d4f
Nathan Campos <eeepc904@gmail.com>
2021-04-09 13:41:22 +0100

    Firmware - Minor change to the way the battery gauge is calculated
    Ref T35 T38

--- Firmware/src/gauge.c
+++ Firmware/src/gauge.c
@@ -26,44 +26,48 @@ void BlinkChargingState(uint8_t *gauge);
  */
 void DisplayBatteryGauge(void) {
 	uint8_t gauge = 0b0000;
-	float voltage = GetCellVoltage();
+	
+	// Only show the gauge if the battery is actually connected.
+	if (!IsBatteryDisconnected()) {
+		float voltage = GetCellVoltage();
 
-	if (IsLithiumBattery()) {
-		/*if (IsFinishedCharging()) {
-			// 100%
-			gauge = ~(0b1111);
-		} else*/ if (voltage > 4.00f) {
-			// 75%
-			gauge = ~(0b0111);
-		} else if (voltage > 3.85f) {
-			// 50%
-			gauge = ~(0b0011);
-		} else if (voltage > 3.75f) {
-			// 25%
-			gauge = ~(0b0001);
+		if (IsLithiumBattery()) {
+			if (IsFinishedCharging()) {
+				// 100%
+				gauge = 0b1111;
+			} else if (voltage > 4.00f) {
+				// 75%
+				gauge = 0b0111;
+			} else if (voltage > 3.85f) {
+				// 50%
+				gauge = 0b0011;
+			} else if (voltage > 3.75f) {
+				// 25%
+				gauge = 0b0001;
+			}
+		} else {
+			if (IsFinishedCharging()) {
+				// 100%
+				gauge = 0b1111;
+			} else if (voltage > 1.42f) {
+				// 75%
+				gauge = 0b0111;
+			} else if (voltage > 1.35f) {
+				// 50%
+				gauge = 0b0011;
+			} else if (voltage > 1.20f) {
+				// 25%
+				gauge = 0b0001;
+			}
 		}
-	} else {
-		/*if (IsFinishedCharging()) {
-			// 100%
-			gauge = ~(0b1111);
-		} else*/ if (voltage > 1.42f) {
-			// 75%
-			gauge = ~(0b0111);
-		} else if (voltage > 1.35f) {
-			// 50%
-			gauge = ~(0b0011);
-		} else if (voltage > 1.20f) {
-			// 25%
-			gauge = ~(0b0001);
-		}
+
+		// Blink them lights.
+		BlinkChargingState(&gauge);
 	}
-
-	// Blink them lights.
-	BlinkChargingState(&gauge);
 
 	// Shift gauge to start at RC2 and push changes to the IO pins.
 	gauge <<= 2;
-	LATC = gauge;
+	LATC = ~(gauge);
 }
 
 /**
@@ -75,16 +79,16 @@ void DisplayBatteryGauge(void) {
 void BlinkChargingState(uint8_t *gauge) {
 	// Determine which bit to flip, making sure we are only getting the first 4 bits
 	switch (*gauge & 0b1111) {
-		case 0b1111:
+		case 0b0000:
 			*gauge ^= blinkState;
 			break;
-		case 0b1110:
+		case 0b0001:
 			*gauge ^= (blinkState << 1);
 			break;
-		case 0b1100:
+		case 0b0011:
 			*gauge ^= (blinkState << 2);
 			break;
-		case 0b1000:
+		case 0b0111:
 			*gauge ^= (blinkState << 3);
 			break;
 	}
